000a86c2e3adb6451580aea89816523bb97c6915
franciscofortes029-byte <francisco.fortes029@gmail.com>
2026-06-19 09:18:28 +0000

    generador dominios francisco

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-import "bootstrap";
-import "./style.css";
+function generadorDominios() {
+    let part1 = ["descubre", "conoce"];
+    let part2 = ["nuestro", "vuestro"];
+    let part3 = ["mundo", "planeta"];
+    let final = [".com", ".es"];
+
+    for (let i = 0; i < part1.length; i++) {
+        for (let j = 0; j < part2.length; j++) {
+            for (let k = 0; k < part3.length; k++) {
+                for (let l = 0; l < final.length; l++) {
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+                    let domain = part1[i] + part2[j] + part3[k] + final[l];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+                    console.log(domain);
+
+                }
+            }
+        }
+    }
+}
+
+generadorDominios();
